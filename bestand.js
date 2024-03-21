11f622110f30d4eaf77d919c5205ea6693c90a49
DasWeltall <151219570+DasWeltall@users.noreply.github.com>
2024-03-21 19:19:15 +0100

    Update bestand.js

--- bestand.js
+++ bestand.js
@@ -12,14 +12,24 @@ let pro8sStock = 1;
 // Funktion, um den Bestand anzuzeigen
 function displayStock() {
     const pro1 = document.getElementById('pro1Stock');
-    const pro2 = document.getElementById('pro1Stock');
-    const pro3 = document.getElementById('pro1Stock');
-    const pro4 = document.getElementById('pro1Stock');
+    const pro2 = document.getElementById('pro2Stock');
+    const pro3 = document.getElementById('pro3Stock');
+    const pro4 = document.getElementById('pro4Stock');
+    const pro5 = document.getElementById('pro5Stock');
+    const pro6 = document.getElementById('pro6Stock');
+    const pro7 = document.getElementById('pro7Stock');
+    const pro8 = document.getElementById('pro8Stock');
  
-    const pro2 = document.getElementById('balls-stock');
+    
 
     pro1.textContent = `Bestand: ${pro1Stock}.`;
     pro2.textContent = `Bestand: ${pro2Stock}.`;
+    pro3.textContent = `Bestand: ${pro3Stock}.`;
+    pro4.textContent = `Bestand: ${pro4Stock}.`;
+    pro5.textContent = `Bestand: ${pro5Stock}.`;
+    pro6.textContent = `Bestand: ${pro6Stock}.`;
+    pro7.textContent = `Bestand: ${pro7Stock}.`;
+    pro8.textContent = `Bestand: ${pro8Stock}.`;
 }
 
 // Rufe die Funktion displayStock auf, um den Bestand anzuzeigen

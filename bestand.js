7661b78b34d1c97f0ae9bf6ac5f01037184a93c6
DasWeltall <151219570+DasWeltall@users.noreply.github.com>
2024-03-22 06:55:29 +0100

    Update bestand.js

--- bestand.js
+++ bestand.js
@@ -1,14 +1,14 @@
 // Annahme: Der aktuelle Bestand ist in Variablen gespeichert
-let flumisStock = 5;
-let ballsStock = 1;
+let pro1Stock = 5;
+let pro2Stock = 3;
 
 // Funktion, um den Bestand anzuzeigen
 function displayStock() {
     const flumisStockElement = document.getElementById('pro1');
     const ballsStockElement = document.getElementById('pro2');
 
-    flumisStockElement.textContent = `Bestand: ${flumisStock} Flumis`;
-    ballsStockElement.textContent = `Bestand: ${ballsStock} Ball`;
+    flumisStockElement.textContent = `Nur ${pro1Stock} auf Lager`;
+    ballsStockElement.textContent = `Nur ${pro2Stock} auf Lager`;
 }
 
 // Rufe die Funktion displayStock auf, um den Bestand anzuzeigen, sobald die Seite geladen ist
